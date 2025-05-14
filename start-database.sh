b9feff5f7a935688973f2c988b4f4a77aede49d0
Eli Ribble <eli@theribbles.org>
2025-05-14 10:48:13 -0700

    Add script to start a database container for local testing

---
+++ start-database.sh
@@ -0,0 +1,9 @@
+#!/run/current-system/sw/bin/bash
+podman run \
+	--name fs-postgres \
+	-e POSTGRES_PASSWORD=letmein \
+	-e POSTGRES_USER=fieldseeker \
+	-p 5432:5432 \
+	--rm \
+	-v ./database:/var/lib/postgresql/data \
+	-d docker.io/postgres:17.5
