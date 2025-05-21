085b637be0c953d659c589126750065912217037
Eli Ribble <eli@theribbles.org>
2025-05-20 20:00:29 -0700

    Move map instantiation to an onload event in the base JS
    ...getting closer to defining it with data from our database.

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,10 @@
-console.log("hey")
+onload = (event) => {
+	console.log("hey")
+	const map = L.map('map').setView([51.505, -0.09], 13);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+	var marker = L.marker([51.5, -0.09]).addTo(map);
+}
