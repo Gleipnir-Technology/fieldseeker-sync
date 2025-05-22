efd58a153c0ad4f6d89274e5e8a282cd7556e66d
Eli Ribble <eli@theribbles.org>
2025-05-22 07:44:07 -0700

    Load templates from disk if they are available
    This tightens our feedback loop so we don't need a recompile when just
    working on HTML content.

--- html/html.go
+++ html/html.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"os"
 
 	"gleipnir.technology/fieldseeker-sync-bridge"
 )
@@ -11,15 +12,26 @@ import (
 //go:embed templates/*
 var files embed.FS
 var (
-	index           = parse("templates/index.html")
-	serviceRequests = parse("templates/service-requests.html")
+	index           = newBuiltTemplate("templates/index.html")
+	serviceRequests = newBuiltTemplate("templates/service-requests.html")
 )
 
+type BuiltTemplate struct {
+	Path     string
+	Template *template.Template
+}
 type PageDataIndex struct {
 	ServiceRequestCount int
 	Title               string
 }
 
+func (bt *BuiltTemplate) ExecuteTemplate(w io.Writer, t string, data any) error {
+	if bt.Template == nil {
+		return parseFromDisk(bt.Path).ExecuteTemplate(w, t, data)
+	} else {
+		return bt.Template.ExecuteTemplate(w, t, data)
+	}
+}
 func Index(w io.Writer, d PageDataIndex) error {
 	return index.ExecuteTemplate(w, "index.html", d)
 }
@@ -32,10 +44,33 @@ func geocode(geo fssync.Geometry) string {
 	return "foo"
 }
 
-func parse(file string) *template.Template {
+func newBuiltTemplate(path string) BuiltTemplate {
+	full_path := "html/" + path
+	_, err := os.Stat(full_path)
+	if err == nil {
+		return BuiltTemplate{
+			Path:     full_path,
+			Template: nil,
+		}
+	}
+	return BuiltTemplate{
+		Path:     path,
+		Template: parseEmbedded(path),
+	}
+}
+
+func parseEmbedded(file string) *template.Template {
 	funcMap := template.FuncMap{
 		"geocode": geocode,
 	}
 	return template.Must(
 		template.New("templates/base.html").Funcs(funcMap).ParseFS(files, "templates/base.html", file))
+}
+
+func parseFromDisk(path string) *template.Template {
+	funcMap := template.FuncMap{
+		"geocode": geocode,
+	}
+	return template.Must(
+		template.New("html/templates/base.html").Funcs(funcMap).ParseFiles(path, "html/templates/base.html", path))
 }
