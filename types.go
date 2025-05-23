fdc9233705cb8a0f4a76a631cd45582a6a5d1d1a
Eli Ribble <eli@theribbles.org>
2025-05-23 15:04:24 -0700

    Fix alphabetization

--- types.go
+++ types.go
@@ -10,8 +10,8 @@ type ServiceRequest struct {
 	Address  *string  `db:"reqaddr1"`
 	City     *string  `db:"reqcity"`
 	Priority *string  `db:"priority"`
-	Status   *string  `db:"status"`
 	Source   *string  `db:"source"`
+	Status   *string  `db:"status"`
 	Target   *string  `db:"reqtarget"`
 	Zip      *string  `db:"reqzip"`
 }
