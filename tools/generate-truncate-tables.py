54f617f94a6c4ba1e8e24874f9d4a1d9e80c597e
Eli Ribble <eli@gleipnir.technology>
2025-05-29 16:28:21 +0000

    Add tool for generating the command to truncate all tables.
    This is only useful when you are going to drop all data by switching
    from a test instance of FieldSeeker data to a production instance, as
    I'm doing right now.

---
+++ tools/generate-truncate-tables.py
@@ -0,0 +1,90 @@
+#!python3
+import argparse
+import dataclasses
+import json
+import logging
+from pathlib import Path
+import sys
+from typing import Any, List, Mapping, Optional
+
+@dataclasses.dataclass
+class Column:
+	name: str
+	type: str
+
+def main() -> None:
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--dest", "-d", type=Path, default="tools/truncate-tables.sql", help="The output file to write to")
+	parser.add_argument("--src", "-s", type=Path, default="schema", help="The directory to pull schema from")
+	args = parser.parse_args()
+
+	logging.basicConfig(level=logging.DEBUG)
+
+	# Parse all the schemas
+	logging.info("Reading schema from %s", args.src)
+	schema: Mapping[str, List[Column]] = {}
+	for src in args.src.glob("*.json"):
+		table_name = src.stem
+		with open(src, "r") as f:
+			data = json.loads(f.read())
+		if "fields" not in data:
+			continue
+		columns = read_schema(data)
+		schema[table_name] = columns
+	
+	logging.info("Writing truncate script to %s", args.dest)
+	with open(args.dest, "w") as output:
+		# Write out what we parsed
+		for table_name in sorted(schema):
+			output.write(f"TRUNCATE TABLE FS_{table_name};\n")
+
+def read_schema(data: Mapping[str, Any]) -> List[Column]:
+	columns = [
+		Column(
+			name="geometry_x",
+			type="FLOAT",
+		),
+		Column(
+			name="geometry_y",
+			type="FLOAT",
+		),
+	]
+			
+	for field in data["fields"]:
+		if field["name"] == data["uniqueIdField"]["name"]:
+			type_ = "INTEGER PRIMARY KEY"
+		else:
+			type_ = column_type(field["type"])
+		columns.append(Column(
+			name=field["name"],
+			type=type_,
+		))
+	return columns
+		
+		
+def column_type(name: str, additional: Optional[str] = None) -> str:
+	result = ""
+	if name == "esriFieldTypeInteger":
+		result = "INT8"
+	elif name == "esriFieldTypeDate":
+		result = "BIGINT"
+	elif name == "esriFieldTypeDouble":
+		result = "DOUBLE PRECISION"
+	elif name == "esriFieldTypeInteger":
+		result = "INT8"
+	elif name == "esriFieldTypeSmallInteger":
+		result = "INT2"
+	elif name == "esriFieldTypeString":
+		result = "TEXT"
+	elif name == "esriFieldTypeGlobalID":
+		result = "TEXT"
+	elif name == "esriFieldTypeGUID":
+		result = "TEXT"
+	elif name == "esriFieldTypeOID":
+		result = "TEXT"
+	else:
+		raise Exception(f"Not sure how to translate {name}")
+	return result + " " + additional if additional else result
+
+if __name__ == "__main__":
+	main()
