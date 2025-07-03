2a33d27679b042f98269b5fae186510563458d91
Eli Ribble <eli@theribbles.org>
2025-07-03 18:32:01 +0000

    Add initial code for starting the Claude code container.
    These were taken from
    https://github.com/anthropics/claude-code/tree/main/.devcontainer

---
+++ claude/start.sh
@@ -0,0 +1,13 @@
+#!/run/current-system/sw/bin/bash
+docker run \
+	--cap-add=NET_ADMIN \
+	--cap-add=NET_RAW \
+	--env NODE_OPTIONS=--max-old-space-size=4096 \
+	--env CLAUDE_CONFIG_DIR=/home/node/.claude \
+	--env POWERLEVEL9K_DISABLE_GITSTATUS=true \
+	-it \
+	--mount "source=claude-code-bashhistory,target=/commandhistory,type=volume" \
+	--mount "source=cloude-code-config,target=/home/node/.claude,type=volume" \
+	--mount "source=./,target=/workspace,type=bind,consistency=delegated" \
+	--name claude \
+	--rm claudecode:latest /bin/bash
