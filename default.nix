2f9a8631f9e1636334bb44ed5c16c9bb41f3ff41
Eli Ribble <eli@theribbles.org>
2025-09-12 18:50:47 +0000

    Bump version to 19, fix tabs

--- default.nix
+++ default.nix
@@ -1,21 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.buildGoModule rec {
-        meta = {
-                description = "FieldSeeker sync";
-                homepage = "https://github.com/Gleipnir-Technology/fieldseeker-sync";
-        };
-        pname = "fieldseeker-sync";
-        src = ./.;
-        subPackages = [
-                "cmd/audio-post-processor"
-                "cmd/download-schema"
-                "cmd/dump"
-                "cmd/full-export"
-                "cmd/login"
-                "cmd/registration"
-                "cmd/webserver"
-        ];
-        version = "0.0.18";
-        # Needs to be updated after every modification of go.mod/go.sum
-        vendorHash = "sha256-0JKxQ+r4ri/BaLVshy47HEGSw5Em6Q2kYY9Z+xNEsZM=";
+	meta = {
+		description = "FieldSeeker sync";
+		homepage = "https://github.com/Gleipnir-Technology/fieldseeker-sync";
+	};
+	pname = "fieldseeker-sync";
+	src = ./.;
+	subPackages = [
+		"cmd/audio-post-processor"
+		"cmd/download-schema"
+		"cmd/dump"
+		"cmd/full-export"
+		"cmd/login"
+		"cmd/registration"
+		"cmd/webserver"
+	];
+	version = "0.0.19";
+	# Needs to be updated after every modification of go.mod/go.sum
+	vendorHash = "sha256-0JKxQ+r4ri/BaLVshy47HEGSw5Em6Q2kYY9Z+xNEsZM=";
 }
