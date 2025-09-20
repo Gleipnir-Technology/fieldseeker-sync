2fbba8289ed6d6ba270d1fddaa9d5e16e7b76486
Eli Ribble <eli@theribbles.org>
2025-09-20 02:05:40 +0000

    Update audio processing functions to work with tasks
    This handles "mark review without changes", and "needs further review"
    
    Still need to fix "Delete"

--- cmd/webserver/endpoint.go
+++ cmd/webserver/endpoint.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/Gleipnir-Technology/fieldseeker-sync"
 	"github.com/Gleipnir-Technology/fieldseeker-sync/database"
-	"github.com/Gleipnir-Technology/fieldseeker-sync/database/models"
 	"github.com/Gleipnir-Technology/fieldseeker-sync/html"
 	"github.com/Gleipnir-Technology/fieldseeker-sync/shared"
 )
@@ -517,149 +516,6 @@ func parseRange(rangeHeader string, fileSize int64) ([]httpRange, error) {
 	return parsedRanges, nil
 }
 
-func processAudioGet(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	sortField := r.URL.Query().Get("sort")
-	var sortEnum database.TaskAudioReviewOutstandingSort
-	switch sortField {
-	case "":
-		sortEnum = database.SortCreated
-	case "created_at":
-		sortEnum = database.SortCreated
-	case "duration":
-		sortEnum = database.SortAudioDuration
-	case "creator":
-		sortEnum = database.SortCreatorName
-	case "needs_review":
-		sortEnum = database.SortNeedsReview
-	default:
-		sortEnum = database.SortCreated
-	}
-	sortOrder := r.URL.Query().Get("order")
-	if sortOrder == "" {
-		sortOrder = "asc"
-	}
-
-	rows, err := database.TaskAudioReviewList(sortEnum, sortOrder == "asc")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//sort.Sort(byReviewedAndAge(tasks))
-	usersById, err := usersById()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	data := html.ContentProcessAudio{
-		Rows:      rows,
-		SortField: sortField,
-		SortOrder: sortOrder,
-		UsersById: usersById,
-		User:      u,
-	}
-
-	err = html.ProcessAudio(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func processAudioIdGet(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	id_str := chi.URLParam(r, "id")
-	id, err := strconv.Atoi(id_str)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	task, err := models.FindTaskAudioReview(context.Background(), database.PGInstance.BobDB, int32(id))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	noteAudio, err := database.NoteAudioGetLatest(context.Background(), task.NoteAudioUUID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	usersById, err := usersById()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	data := html.ContentProcessAudioId{
-		NoteAudio: noteAudio,
-		Task:      task,
-		UsersById: usersById,
-		User:      u,
-	}
-
-	log.Printf("noteAudio %s isvalue %v", noteAudio.UUID, noteAudio.Transcription.IsNull())
-	err = html.ProcessAudioId(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func processAudioIdPost(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	id_str := chi.URLParam(r, "id")
-
-	id, err := strconv.Atoi(id_str)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	task, err := models.FindTaskAudioReview(context.Background(), database.PGInstance.BobDB, int32(id))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	r.ParseForm()
-	transcription := r.Form.Get("transcription")
-	log.Printf("Updating %s to transcript %s", task.NoteAudioUUID, transcription)
-	err = database.NoteAudioUpdateTranscription(task.NoteAudioUUID, transcription, u.ID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/process-audio", http.StatusFound)
-}
-
-func processAudioIdDeletePost(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	uuid := chi.URLParam(r, "uuid")
-	log.Printf("Deleting %s", uuid)
-	err := database.NoteAudioUpdateDelete(uuid, u.ID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/process-audio", http.StatusFound)
-}
-
-func processAudioIdReviewedPost(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	uuid := chi.URLParam(r, "uuid")
-	log.Printf("Updating %s to reviewed without changes", uuid)
-	err := database.NoteAudioUpdateReviewed(uuid)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/process-audio", http.StatusFound)
-}
-
-func processAudioIdNeedsFurtherReviewPost(w http.ResponseWriter, r *http.Request, u *shared.User) {
-	uuid := chi.URLParam(r, "uuid")
-	log.Printf("Updating %s to needs further review", uuid)
-	err := database.NoteAudioUpdateNeedsFurtherReview(uuid)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/process-audio", http.StatusFound)
-}
-
 func usersById() (map[int]*shared.User, error) {
 	users, err := database.Users()
 	if err != nil {
@@ -670,18 +526,4 @@ func usersById() (map[int]*shared.User, error) {
 		usersById[u.ID] = u
 	}
 	return usersById, nil
-}
-
-type byReviewedAndAge []*models.TaskAudioReview
-
-func (a byReviewedAndAge) Len() int      { return len(a) }
-func (a byReviewedAndAge) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a byReviewedAndAge) Less(i, j int) bool {
-	if a[i].NeedsReview == a[j].NeedsReview {
-		if a[i].ReviewedBy == a[j].ReviewedBy {
-			return a[i].Created.Before(a[j].Created)
-		}
-		return a[i].ReviewedBy.IsNull()
-	}
-	return a[i].NeedsReview
 }
--- database/note_audio.go
+++ database/note_audio.go
@@ -5,15 +5,125 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/Gleipnir-Technology/fieldseeker-sync/database/models"
-
+	"github.com/Gleipnir-Technology/fieldseeker-sync/shared"
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	//"github.com/stephenafamo/bob"
 	"github.com/stephenafamo/bob/dialect/psql"
 	//"github.com/stephenafamo/bob/dialect/psql/dialect"
 	"github.com/stephenafamo/bob/dialect/psql/sm"
 )
+
+func NoteAudioCreate(ctx context.Context, noteUUID uuid.UUID, payload shared.NoteAudioPayload, userID int) error {
+	var options pgx.TxOptions
+	transaction, err := PGInstance.DB.BeginTx(ctx, options)
+	if err != nil {
+		return fmt.Errorf("Failed to begin transaction: %v", err)
+	}
+
+	query := `INSERT INTO note_audio (
+			created,
+			creator,
+			deleted,
+			duration,
+			has_been_reviewed,
+			is_audio_normalized,
+			is_transcoded_to_ogg,
+			needs_further_review,
+			transcription,
+			transcription_internally_edited,
+			transcription_user_edited,
+			version,
+			uuid
+		) VALUES (
+			@created,
+			@creator,
+			@deleted,
+			@duration,
+			@has_been_reviewed,
+			@is_audio_normalized,
+			@is_transcoded_to_ogg,
+			@needs_further_review,
+			@transcription,
+			@transcription_internally_edited,
+			@transcription_user_edited,
+			@version,
+			@uuid)`
+	args := pgx.NamedArgs{
+		"created":                         payload.Created,
+		"creator":                         userID,
+		"deleted":                         nil,
+		"duration":                        payload.Duration,
+		"has_been_reviewed":               false,
+		"is_audio_normalized":             false,
+		"is_transcoded_to_ogg":            false,
+		"needs_further_review":            false,
+		"transcription":                   payload.Transcription,
+		"transcription_internally_edited": false,
+		"transcription_user_edited":       payload.TranscriptionUserEdited,
+		"version":                         payload.Version,
+		"uuid":                            noteUUID,
+	}
+	row, err := PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		transaction.Rollback(ctx)
+		return fmt.Errorf("Unable to insert row into note_audio: %v", err)
+	}
+	log.Println("Saved audio note", noteUUID, row)
+
+	rows := make([][]interface{}, 0, len(payload.Breadcrumbs))
+	for i, b := range payload.Breadcrumbs {
+		rows = append(rows, []interface{}{
+			b.Created,
+			b.Cell,
+			noteUUID,
+			payload.Version,
+			i,
+		})
+	}
+
+	PGInstance.DB.CopyFrom(
+		ctx,
+		pgx.Identifier{"note_audio_breadcrumb"},
+		[]string{"created", "cell", "note_audio_uuid", "note_audio_version", "position"},
+		pgx.CopyFromRows(rows),
+	)
+
+	query = `INSERT INTO task_audio_review (
+		completed_by,
+		needs_review,
+		note_audio_uuid,
+		note_audio_version,
+		reviewed_by
+	) VALUES (
+		@completed_by,
+		@needs_review,
+		@note_audio_uuid,
+		@note_audio_version,
+		@reviewed_by)`
+	args = pgx.NamedArgs{
+		"completed_by":       nil,
+		"needs_review":       false,
+		"note_audio_uuid":    noteUUID,
+		"note_audio_version": payload.Version,
+		"reviewed_by":        nil,
+	}
+	row, err = PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		transaction.Rollback(ctx)
+		return fmt.Errorf("Unable to insert row into task_audio_review: %v", err)
+	}
+
+	err = transaction.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to commit transaction: %v", err)
+	}
+	return nil
+}
 
 func NoteAudioGetLatest(ctx context.Context, uuid string) (*models.NoteAudio, error) {
 	if PGInstance == nil {
@@ -26,6 +136,198 @@ func NoteAudioGetLatest(ctx context.Context, uuid string) (*models.NoteAudio, er
 	).One(ctx, PGInstance.BobDB)
 }
 
+func NoteAudioNormalized(uuid string) error {
+	if PGInstance == nil {
+		return errors.New("You must initialize the DB first")
+	}
+	args := pgx.NamedArgs{
+		"is_audio_normalized": true,
+		"uuid":                uuid,
+	}
+	query := "UPDATE note_audio SET is_audio_normalized=@is_audio_normalized WHERE uuid=@uuid"
+	_, err := PGInstance.DB.Exec(context.Background(), query, args)
+	return err
+}
+
+func NoteAudioQuery() ([]*shared.NoteAudio, error) {
+	results := make([]*shared.NoteAudio, 0)
+	if PGInstance == nil {
+		return results, errors.New("You must initialize the DB first")
+	}
+	query := `
+		SELECT 
+			created,
+			creator,
+			duration,
+			has_been_reviewed,
+			is_audio_normalized,
+			is_transcoded_to_ogg,
+			needs_further_review,
+			transcription,
+			transcription_user_edited,
+			version,
+			uuid
+		FROM (
+			SELECT *, ROW_NUMBER() OVER (PARTITION BY uuid ORDER BY version DESC) as version_rank
+			FROM note_audio
+		) ranked_rows
+		WHERE version_rank = 1 AND deleted IS NULL;
+	`
+	rows, _ := PGInstance.DB.Query(context.Background(), query)
+
+	if err := pgxscan.ScanAll(&results, rows); err != nil {
+		log.Println("ScanAll on note_audio error:", err)
+		return results, err
+	}
+	return results, nil
+}
+
+func NoteAudioToNormalize() ([]*shared.NoteAudio, error) {
+	results := make([]*shared.NoteAudio, 0)
+	if PGInstance == nil {
+		return results, errors.New("You must initialize the DB first")
+	}
+	args := pgx.NamedArgs{}
+	query := "SELECT created, creator, duration, is_audio_normalized, transcription, transcription_user_edited, version, uuid FROM note_audio WHERE is_audio_normalized = FALSE"
+
+	rows, _ := PGInstance.DB.Query(context.Background(), query, args)
+
+	if err := pgxscan.ScanAll(&results, rows); err != nil {
+		log.Println("ScanAll on note_audio error:", err)
+		return results, err
+	}
+	return results, nil
+}
+
+func NoteAudioToTranscodeToOgg() ([]*shared.NoteAudio, error) {
+	results := make([]*shared.NoteAudio, 0)
+	if PGInstance == nil {
+		return results, errors.New("You must initialize the DB first")
+	}
+	args := pgx.NamedArgs{}
+	query := "SELECT created, creator, duration, is_audio_normalized, transcription, transcription_user_edited, version, uuid FROM note_audio WHERE is_transcoded_to_ogg = FALSE"
+
+	rows, _ := PGInstance.DB.Query(context.Background(), query, args)
+
+	if err := pgxscan.ScanAll(&results, rows); err != nil {
+		log.Println("ScanAll on note_audio error:", err)
+		return results, err
+	}
+	return results, nil
+}
+
+func NoteAudioTranscodedToOgg(uuid string) error {
+	if PGInstance == nil {
+		return errors.New("You must initialize the DB first")
+	}
+	args := pgx.NamedArgs{
+		"is_transcoded_to_ogg": true,
+		"uuid":                 uuid,
+	}
+	query := "UPDATE note_audio SET is_transcoded_to_ogg=@is_transcoded_to_ogg WHERE uuid=@uuid"
+	_, err := PGInstance.DB.Exec(context.Background(), query, args)
+	return err
+}
+
+func NoteAudioUpdateDelete(uuid string, userID int) error {
+	ctx := context.Background()
+	var options pgx.TxOptions
+	transaction, err := PGInstance.DB.BeginTx(ctx, options)
+
+	if err != nil {
+		return fmt.Errorf("Failed to begin transaction: %v", err)
+	}
+	args := pgx.NamedArgs{
+		"deleted":    time.Now(),
+		"deleted_by": userID,
+		"uuid":       uuid,
+	}
+	query := `
+		UPDATE note_audio
+		SET deleted=@deleted, deleted_by=@deleted_by
+		WHERE uuid=@uuid
+	`
+	row, err := PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update note_audio to deleted: %v\n", err)
+	}
+
+	query = `UPDATE task_audio_review SET
+		completed_by=@completed_by
+		WHERE note_audio_uuid=@note_audio_uuid`
+	args = pgx.NamedArgs{
+		"completed_by":    userID,
+		"note_audio_uuid": uuid,
+	}
+	row, err = PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update note_audio to deleted: %v\n", err)
+	}
+
+	log.Printf("Marked task_audio_review for %s %s completed", uuid, row)
+	err = transaction.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to commit transaction: %v", err)
+	}
+	return nil
+}
+
+func NoteAudioUpdateReviewed(uuid string, userID int) error {
+	args := pgx.NamedArgs{
+		"has_been_reviewed": true,
+		"user_id":           userID,
+		"uuid":              uuid,
+	}
+	query := `
+		UPDATE note_audio
+		SET has_been_reviewed=@has_been_reviewed
+		WHERE uuid=@uuid
+	`
+	row, err := PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update transcription: %v\n", err)
+	}
+	log.Printf("Marked note_audio %s %s reviewed", uuid, row)
+	query = `UPDATE task_audio_review SET
+		completed_by=@user_id WHERE note_audio_uuid=@uuid`
+	row, err = PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update transcription: %v\n", err)
+	}
+	log.Printf("Marked review task for %s %s completed by %s", uuid, userID, row)
+
+	return nil
+}
+
+func NoteAudioUpdateNeedsFurtherReview(uuid string, userID int) error {
+	args := pgx.NamedArgs{
+		"needs_further_review": true,
+		"user_id":              userID,
+		"uuid":                 uuid,
+	}
+	query := `
+		UPDATE note_audio
+		SET needs_further_review=@needs_further_review
+		WHERE uuid=@uuid
+	`
+	row, err := PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update needs further review: %v\n", err)
+	}
+	log.Printf("Marked note_audio %s %s needs further review", uuid, row)
+
+	query = `
+		UPDATE task_audio_review SET
+		completed_by=@user_id,
+		needs_review=@needs_further_review
+		WHERE note_audio_uuid=@uuid
+	`
+	row, err = PGInstance.DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Failed to update needs further review on review task: %v\n", err)
+	}
+	return nil
+}
 func NoteAudioUpdateTranscription(uuid string, transcription string, userUUID int) error {
 	ctx := context.Background()
 	var options pgx.TxOptions
