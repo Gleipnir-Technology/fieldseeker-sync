53cc38dee309cb4e782b171ebad9d32e3ed13cbf
Eli Ribble <eli@theribbles.org>
2025-10-30 15:47:35 +0000

    Fix broken error messages
    Found through static analysis.

--- user_files.go
+++ user_files.go
@@ -30,15 +30,15 @@ func AudioFileContentWrite(audioUUID uuid.UUID, body io.Reader) error {
 	filepath := AudioFileContentPathRaw(audioUUID.String())
 	dst, err := os.Create(filepath)
 	if err != nil {
-		log.Printf("Failed to create audio file at %s: %v\n", dst, err)
-		return fmt.Errorf("Failed to create audio file at %s: %v", dst, err)
+		log.Printf("Failed to create audio file at %s: %v\n", filepath, err)
+		return fmt.Errorf("Failed to create audio file at %s: %v", filepath, err)
 	}
 	defer dst.Close()
 
 	// Copy rest of request body to file
 	_, err = io.Copy(dst, body)
 	if err != nil {
-		return fmt.Errorf("Unable to save file to create audio file at %s: %v", dst, err)
+		return fmt.Errorf("Unable to save file to create audio file at %s: %v", filepath, err)
 	}
 	log.Printf("Saved audio content to %s\n", filepath)
 	return nil
